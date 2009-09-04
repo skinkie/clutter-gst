4162ad998584593af0cff252766b31ea2f1bded8
Damien Lespiau <damien.lespiau@intel.com>
2009-09-05 00:04:04 +0100

    [VideoSink] Make the alpha component of the GL color play with the shaders
    The idea here is to generate a premultiplied alpha texture of the video
    so the user can blend it in a clutter scene and do some fancy effects.
    Through the use of clutter_actor_set_opacity() on the texture, we are
    now able to make a video transparent.
    
    This is for the YUV -> RGB shaders only for now. RGB uploads + alpha
    will have to wait a bit.

--- clutter-gst/shaders/I420.h
+++ clutter-gst/shaders/I420.h
@@ -9,26 +9,28 @@
  * This define is the size of the shader in bytes.  More precisely it's the
  * sum of strlen() of every string in the array.
  */
-#define I420_FP_SZ    526
+#define I420_FP_SZ    564
 
 static const char *I420_fp[] =
 {
   "!!ARBfp1.0\n",
-  "PARAM c[2] = { { 1, 1.1640625, 0.0625, 2.015625 },\n",
-  "{ 0.5, 0.390625, 0.8125, 1.5976562 } };\n",
+  "PARAM c[2] = { { 1.1640625, 0.0625, 2.015625, 0.5 },\n",
+  "{ 0.390625, 0.8125, 1.5976562 } };\n",
   "TEMP R0;\n",
-  "TEX R0.y, fragment.texcoord[0], texture[0], 2D;\n",
-  "ADD R0.x, R0.y, -c[0].z;\n",
-  "TEX R0.y, fragment.texcoord[0], texture[2], 2D;\n",
-  "ADD R0.z, R0.y, -c[1].x;\n",
-  "MUL R0.x, R0, c[0].y;\n",
-  "MAD result.color.z, R0, c[0].w, R0.x;\n",
+  "TEMP R1;\n",
+  "TEX R1.y, fragment.texcoord[0], texture[0], 2D;\n",
+  "ADD R0.x, R1.y, -c[0].y;\n",
   "TEX R0.y, fragment.texcoord[0], texture[1], 2D;\n",
-  "ADD R0.y, R0, -c[1].x;\n",
-  "MAD R0.z, -R0, c[1].y, R0.x;\n",
-  "MAD result.color.y, -R0, c[1].z, R0.z;\n",
-  "MAD result.color.x, R0.y, c[1].w, R0;\n",
-  "MOV result.color.w, c[0].x;\n",
+  "TEX R1.y, fragment.texcoord[0], texture[2], 2D;\n",
+  "MUL R0.x, R0, c[0];\n",
+  "ADD R0.w, R1.y, -c[0];\n",
+  "MAD R0.z, R0.w, c[0], R0.x;\n",
+  "ADD R0.y, R0, -c[0].w;\n",
+  "MAD R0.w, -R0, c[1].x, R0.x;\n",
+  "MAD R0.x, R0.y, c[1].z, R0;\n",
+  "MAD R0.y, -R0, c[1], R0.w;\n",
+  "MUL result.color.xyz, fragment.color.primary.w, R0;\n",
+  "MOV result.color.w, fragment.color.primary;\n",
   "END\n",
   NULL
 };
