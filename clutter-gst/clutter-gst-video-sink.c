ec6604f60de31b375e3206cc084127685560e40c
György Balló <ballogy@freestart.hu>
2011-10-03 12:08:45 +0100

    video-sink: Fix segmentation fault with nouveau mesa driver
    Partially revert commit f92a80288a57cc0fbc58a74e3a143b7318649557
    This fixes video playback on systems that uses the nouveau classic mesa driver.
    
    You can't query an int field of a GstStructure as uint.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=660732

--- clutter-gst/clutter-gst-video-sink.c
+++ clutter-gst/clutter-gst-video-sink.c
@@ -1028,7 +1028,7 @@ clutter_gst_video_sink_set_caps (GstBaseSink *bsink,
   const GValue               *par;
   gint                        width, height;
   guint32                     fourcc;
-  guint                       red_mask, blue_mask;
+  int                         red_mask, blue_mask;
 
   sink = CLUTTER_GST_VIDEO_SINK(bsink);
   priv = sink->priv;
@@ -1094,8 +1094,8 @@ clutter_gst_video_sink_set_caps (GstBaseSink *bsink,
   else
     {
       guint32 mask;
-      gst_structure_get_uint (structure, "red_mask", &red_mask);
-      gst_structure_get_uint (structure, "blue_mask", &blue_mask);
+      gst_structure_get_int (structure, "red_mask", &red_mask);
+      gst_structure_get_int (structure, "blue_mask", &blue_mask);
 
       mask = red_mask | blue_mask;
       if (mask < 0x1000000)
